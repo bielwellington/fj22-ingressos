4dfab6b52e12d05132e4d782a957905d6d4faa0c
Gabriel Wellington <gabrielwellington@live.com>
2018-09-22 12:42:01 -0300

    Foi realizada criação da class SessaoForm.

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -1,13 +1,21 @@
 package br.com.caelum.ingresso.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.ingresso.dao.FilmeDao;
 import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.dao.SessaoDao;
+import br.com.caelum.ingresso.model.Sessao;
+import br.com.caelum.ingresso.model.form.SessaoForm;
 
 @Controller
 public class SessaoController {
@@ -16,16 +24,31 @@ public class SessaoController {
 	private SalaDao salaDao;
 	@Autowired
 	private FilmeDao filmeDao;
+	@Autowired
+	private SessaoDao sessaoDao;
 	
 	@GetMapping("/admin/sessao")
-	public ModelAndView form(@RequestParam("salaId") Integer salaId){
+	public ModelAndView form(@RequestParam("salaId") Integer salaId, SessaoForm form){
 		
-		ModelAndView modelandview = new ModelAndView();
+		form.setSalaId(salaId);
+		
+		ModelAndView modelandview = new ModelAndView("sessao/sessao");
 		
 		modelandview.addObject("sala", salaDao.findOne(salaId));
 		modelandview.addObject("filmes", filmeDao.findAll());
+		modelandview.addObject("form",form);
 		
 		return modelandview;
+	}
+	
+	@PostMapping(value = "/admin/sessao")
+	@Transactional
+	public ModelAndView salva(@Valid SessaoForm form, BindingResult result){
+		
+		if(result.hasErrors()) return form(form.getSalaId(),form);
+		Sessao sessao = form.toSessao(salaDao, filmeDao);
+		sessaoDao.save(sessao);
+		return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
 	}
 	
 }
