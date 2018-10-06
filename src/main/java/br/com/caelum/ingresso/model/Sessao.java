9bad9ccd4c5635c3a037503fd3cfbe598eb0f53a
Gabriel Wellington <gabrielwellington@live.com>
2018-10-06 10:32:27 -0300

    Finalizado a seleção de lugares, ingresso e tipo de ingresso.

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -3,13 +3,17 @@ package br.com.caelum.ingresso.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.time.LocalTime;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Sessao {
@@ -24,6 +28,9 @@ public class Sessao {
 	private Filme filme;
 	private BigDecimal preco = new BigDecimal(0);
 	
+	@OneToMany(mappedBy="sessao", fetch=FetchType.EAGER)
+	private Set<Ingresso> ingressos = new HashSet<>();
+	
 	public Sessao(){
 	}
 
@@ -32,6 +39,10 @@ public class Sessao {
 		this.filme = filme;
 		this.sala = sala;
 		this.preco = sala.getPreco().add(filme.getPreco());
+	}
+	
+	public boolean isDisponivel(Lugar lugarSelecionado){
+		return ingressos.stream().map(Ingresso::getLugar).noneMatch(lugar -> lugar.equals(lugarSelecionado));
 	}
 	
     public BigDecimal getPreco(){
